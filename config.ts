0dc4cd1a58198b64f1218c60bebfce604fb44b65
Daniel Mathews <dannyl.mathews@gmail.com>
2025-04-10 13:47:40 -0700

    use new endpoint

--- config.ts
+++ config.ts
@@ -8,7 +8,7 @@ const developmentConfig = {
 }
 
 const developmentConfigButProductionData = {
-  "SERVER_URL": "https://api-preview.inkverse.co/api/graphql",
+  "SERVER_URL": "https://api-v2.inkverse.co",
   "SENTRY_URL": "https://09be2495177f48c48f6161ad3b37949a@o4504175906455553.ingest.sentry.io/4504175947612160",
   "POST_HOG_INFO": {
     API_KEY: '0000000000',
@@ -17,7 +17,7 @@ const developmentConfigButProductionData = {
 }
 
 const productionConfig = {
-  "SERVER_URL": "https://api-preview.inkverse.co/api/graphql",
+  "SERVER_URL": "https://api-v2.inkverse.co",
   "SENTRY_URL": "https://c295077d608f4d67835c2391ee0a688d@o4504175906455553.ingest.sentry.io/4504175951544320",
   "POST_HOG_INFO": {
     API_KEY: 'phc_ADit78DdDgFCBzE0qksQOat2x8xn4NfISUdVtmkArWD',
@@ -27,4 +27,4 @@ const productionConfig = {
 
 export default process.env.NODE_ENV === 'production'  
   ? productionConfig
-  : developmentConfigButProductionData
+  : developmentConfig
